cafc76f71e893d6f52f6cfcca7faa778aa0f2736
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-07-18 09:10:27 +0900

    style: lombok에서 필요한 어노테이션만 임포트 하도록 변경

--- src/main/java/com/booklog/payment/product/domain/ProductDto.java
+++ src/main/java/com/booklog/payment/product/domain/ProductDto.java
@@ -1,8 +1,10 @@
 package com.booklog.payment.product.domain;
 
-import lombok.*;
-
-import java.time.OffsetDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -10,16 +12,17 @@ import java.time.OffsetDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductDto {
-    private long sellerId;
-    private long bookId;
-    private String name;
-    private int price;
-    private int condition;
-    private int deliveryPoss;
-    private int tradeStatus;
-    private String description;
+	private long productId;
+	private long sellerId;
+	private long bookId;
+	private String name;
+	private int price;
+	private int condition;
+	private int deliveryPoss;
+	private int tradeStatus;
+	private String description;
 
-    // nullable Column
-    private int deliveryFee = -1;
-    private String tradeLocation = null;
+	// nullable Column
+	private int deliveryFee = -1;
+	private String tradeLocation = null;
 }
