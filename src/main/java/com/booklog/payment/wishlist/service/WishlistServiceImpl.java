0263761d65bff8fb9fae165fb12959db9cbde255
Jeong-Hoon-Yang <yjhoon3658@naver.com>
2023-07-19 00:01:38 +0900

    refactor: 변수 명을 조금 더 알기 쉽도록 변경

--- src/main/java/com/booklog/payment/wishlist/service/WishlistServiceImpl.java
+++ src/main/java/com/booklog/payment/wishlist/service/WishlistServiceImpl.java
@@ -1,6 +1,9 @@
 package com.booklog.payment.wishlist.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,61 +11,53 @@ import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.data.redis.core.SetOperations;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class WishlistServiceImpl implements WishlistService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+	private final RedisTemplate<String, Object> redisTemplate;
 
-    @Autowired
-    public WishlistServiceImpl(RedisTemplate redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
+	@Override
+	public List<Object> wishProduct(Map<String, Long> map) {
+		return redisTemplate.execute(new SessionCallback<>() {
+			@Override
+			public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
+				operations.multi();
+				String productKey = ("wishlist:product-to-user:" + map.get("productNo"));
+				operations.opsForSet().add((K)productKey, (V)map.get("userNo"));
+				String userKey = ("wishlist:user-to-product:" + map.get("userNo"));
+				operations.opsForSet().add((K)userKey, (V)map.get("productNo"));
+				return operations.exec();
+			}
+		});
+	}
 
-    @Override
-    public List wishProduct(Map<String, String> map) {
-        List<Object> execute = redisTemplate.execute(new SessionCallback<>() {
-            @Override
-            public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
-                operations.multi();
-                String key = "wishlist:product-to-user:" + map.get("productNo");
-                operations.opsForSet().add((K) key, (V) map.get("userNo"));
-                key = "wishlist:user-to-product:" + map.get("userNo");
-                operations.opsForSet().add((K) key, (V) map.get("productNo"));
-                return operations.exec();
-            }
-        });
-        return execute;
-    }
+	@Override
+	public Long getProductWishNum(String productNo) {
+		SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+		return setOperations.size("wishlist:product-to-user:" + productNo);
+	}
 
-    @Override
-    public Long getProductWishNum(String productNo) {
-        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
-        return setOperations.size("wishlist:product-to-user:" + productNo);
-    }
+	@Override
+	public Set<Object> getWishList(String userNo) {
+		SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
+		return setOperations.members("wishlist:user-to-product:" + userNo);
+	}
 
-    @Override
-    public Set getWishList(String userNo) {
-        SetOperations<String, Object> setOperations = redisTemplate.opsForSet();
-        return setOperations.members("wishlist:user-to-product:" + userNo);
-    }
-
-    @Override
-    public List unwishProduct(Map<String, String> map) {
-        List<Object> execute = redisTemplate.execute(new SessionCallback<>() {
-            @Override
-            public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
-                operations.multi();
-                String key = "wishlist:product-to-user:" + map.get("productNo");
-                operations.opsForSet().remove((K) key, (V) map.get("userNo"));
-                key = "wishlist:user-to-product:" + map.get("userNo");
-                operations.opsForSet().remove((K) key, (V) map.get("productNo"));
-                return operations.exec();
-            }
-        });
-        return execute;
-    }
+	@Override
+	public List<Object> unwishProduct(Map<String, String> map) {
+		return redisTemplate.execute(new SessionCallback<>() {
+			@Override
+			public <K, V> List<Object> execute(RedisOperations<K, V> operations) throws DataAccessException {
+				operations.multi();
+				String productKey = "wishlist:product-to-user:" + map.get("productNo");
+				operations.opsForSet().remove((K)productKey, (V)map.get("userNo"));
+				String userKey = "wishlist:user-to-product:" + map.get("userNo");
+				operations.opsForSet().remove((K)userKey, (V)map.get("productNo"));
+				return operations.exec();
+			}
+		});
+	}
 }
